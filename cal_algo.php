bce3e1f10be4de9cac68ab42e431cd6b2a2b9052
gimmesomecoffe <ogrod0808@gmail.com>
2022-03-22 21:04:48 +0100

    added css

--- cal_algo.php
+++ cal_algo.php
@@ -4,48 +4,57 @@ class Calendar{
 
 
 
-    public function __construct(){
-      $this->show();
+
+    public function __construct($year, $month){
+      $this->show($year, $month);
 
     }
-    public function show(){
-        $this->yearInfo(3,2022);
+    public function show($year, $month){
+        $this->yearInfo($month,$year);
         $this->createNav();
-        $this->createCalendarCard(3,2022);
+        $this->createCalendarCard($month,$year);
 
 
     }
     private $days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
 
+
+
     public function yearInfo($month, $year){
         $month = date("F", $month);
-        echo "<main><p>$year $month </p>";
+        echo "<h1>$year $month </h1>";
     }
 
-    public function createNav(){
-        echo "<table style='color:blue; border:1px solid blue; width:500px; height:300px;'> <tr>";
+    public function createNav(){ // Metoda tworzy naglowki dla tabeli
+        echo "<table> <tr>";
         foreach($this->days as $value){
-            echo "<td> $value </td>";
+
+            if ($value=="Sun")
+            {
+                echo "<th class='sunRedTh'> $value </th>";
+            }
+            else{
+                echo "<th> $value </th>";
+            }
         }
         echo"</tr>";
     }
 
 
-    public function createCalendarCard($month, $year){
+    public function createCalendarCard($month, $year){  //Metoda odpowiadajaca za generowanie dni w kalendarzu
          $date = mktime(12, 0, 0, $month, 1, $year);
          $daysNumber = cal_days_in_month(CAL_GREGORIAN, $month, $year);
 
          $offset = date('w', $date);
         $rowNumber = 1;
-        for($i = 1; $i <= $offset; $i++)
+        for($i = 1; $i <= $offset; $i++) // jezeli miesiac nie rozpoczyna sie od poniedzialku to dodaje puste rekordy
         {
             echo "<td></td>";
 
         }
 
-        for($day = 1; $day <= $daysNumber; $day++)
+        for($day = 1; $day <= $daysNumber; $day++) // dodaje dni, jezeli dojdzie do 7miu to zaczyna nowy wiersz
         {
-            $datka = $year."-".$month."-".$day;
             $dayInNumber = date("w", mktime(0,0,0,$month,$day,$year));
 
             if( ($day + $offset - 1) % 7 == 0 && $day != 1)
@@ -54,13 +63,13 @@ class Calendar{
                 $rowNumber++;
             }
             if ($dayInNumber == 6) {
-                echo "<td style='color: red;'>" . $day . "</td>";
+                echo "<td class='sunRedTd''>" . $day . "</td>";
             } else{
                 echo "<td>" . $day . "</td>";
             }
 
         }
-        while( ($day + $offset) <= $rowNumber * 7)
+        while( ($day + $offset) <= $rowNumber * 7) // jezeli dni w miesiacu sie skonczyly, a rzadek nie doszedl do konca to dodaje puste rekordy
         {
 
             echo "<td></td>";
@@ -68,7 +77,7 @@ class Calendar{
 
             $day++;
         }
-        echo "</tr></table></main>";
+        echo "</tr></table>";
 
     }
 
--- index.php
+++ index.php
@@ -4,10 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <title>d</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-
+<main>
+<form action="" method="POST">
+    Enter date (numeric month, from 1-12): <br>
+    <input type="number" name="year" value="year" placeholder="year"><br>
+    <input type="number" name="month" value="month" placeholder="month" ><br>
+    <input type="submit" name="submit" value="submit"><br>
+</form>
 
 
 
@@ -15,12 +22,12 @@
 <?php
 include"cal_algo.php";
 
-$calendar = new Calendar();
+$calendar = new Calendar($_POST['year'], $_POST['month']);
 
 
 ?>
 
-
+</main>
 
 
 
