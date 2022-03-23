69942750a12c2c88ba79a570ddbcce9cf62a3ec8
gimmesomecoffe <ogrod0808@gmail.com>
2022-03-23 22:03:59 +0100

    fixed

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 <main>
 <form action="" method="POST">
     Enter date (numeric month, from 1-12): <br>
-    <input type="number" name="year" value="year" placeholder="year"><br>
-    <input type="number" name="month" value="month" placeholder="month" ><br>
+    <input type="number" name="year" value="year" placeholder="year" required><br>
+    <input type="number" name="month" value="month" placeholder="month" required max="12"><br>
     <input type="submit" name="submit" value="submit"><br>
 </form>
 
@@ -22,7 +22,10 @@
 <?php
 include"cal_algo.php";
 
-$calendar = new Calendar($_POST['year'], $_POST['month']);
+if(!empty( $_POST['month'])){
+    $calendar = new Calendar($_POST['year'], $_POST['month']);
+}
+
 
 
 ?>
